e7e2a51108baa869d6ebb2477b9c2eddfd061af0
RM <zapperhatz26@gmail.com>
2018-06-11 13:24:20 +0800

    updated broken link

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ $host = '122.3.33.110';
 if($socket =@ fsockopen($host, 3080, $errno, $errstr, 30)) {
 fclose($socket);
 ?>
-<iframe src="https://122.3.33.110:3080"></iframe>
+<iframe src="http://122.3.33.110:3080"></iframe>
 <?php
 
 } else {
